f416e8bab8ff856add7370dbd52bdcbb834e5ccb
Paulette <paulettehm0421@gmail.com>
2023-09-04 19:13:28 -0600

    Deployment

---
+++ precache-manifest.1c2c7f1593a666328fa9f010d49698ad.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "b163739ae79eccf82282",
+    "url": "/myStock/static/css/main.f1a20c29.chunk.css"
+  },
+  {
+    "revision": "b163739ae79eccf82282",
+    "url": "/myStock/static/js/main.b74eea67.chunk.js"
+  },
+  {
+    "revision": "59b40dd99a3f624c8399",
+    "url": "/myStock/static/js/runtime~main.05b6fbfd.js"
+  },
+  {
+    "revision": "3f8628644d606885639c",
+    "url": "/myStock/static/css/2.1442fb4d.chunk.css"
+  },
+  {
+    "revision": "3f8628644d606885639c",
+    "url": "/myStock/static/js/2.10420974.chunk.js"
+  },
+  {
+    "revision": "b4b4871b09030034e58525d99877bc27",
+    "url": "/myStock/static/media/error.b4b4871b.png"
+  },
+  {
+    "revision": "220075578355b38d8a87ad3d4395da66",
+    "url": "/myStock/static/media/profilepicture.22007557.png"
+  },
+  {
+    "revision": "69c9fea739849259527bce5f23153286",
+    "url": "/myStock/static/media/plus.69c9fea7.png"
+  },
+  {
+    "revision": "6f891179e57e1f472755a4f3572d8478",
+    "url": "/myStock/static/media/upload.6f891179.png"
+  },
+  {
+    "revision": "55c319aae5796df0c767719e781b62e8",
+    "url": "/myStock/static/media/notification.55c319aa.png"
+  },
+  {
+    "revision": "6c7d083fc17540c4f4c44a6734b6ec73",
+    "url": "/myStock/static/media/logo.6c7d083f.png"
+  },
+  {
+    "revision": "fce5365c9b80376e4116894ce595737e",
+    "url": "/myStock/static/media/arrow.fce5365c.png"
+  },
+  {
+    "revision": "6302082f922346e8a538f9773345a0db",
+    "url": "/myStock/static/media/search.6302082f.png"
+  },
+  {
+    "revision": "564111d450453f02a77bd472402b3a1a",
+    "url": "/myStock/static/media/account.564111d4.png"
+  },
+  {
+    "revision": "f18954df1c5f59cb8b5a7d3621e0b9c4",
+    "url": "/myStock/static/media/profits.f18954df.png"
+  },
+  {
+    "revision": "a5ee7972b40c409faf8e0d8f83c89233",
+    "url": "/myStock/static/media/settings.a5ee7972.png"
+  },
+  {
+    "revision": "8fb1a997c6705bcf32923bcd4dcbf379",
+    "url": "/myStock/static/media/pdf.8fb1a997.png"
+  },
+  {
+    "revision": "5683f671b437a7be353f627dab04d4a0",
+    "url": "/myStock/static/media/user.5683f671.png"
+  },
+  {
+    "revision": "c08d00cac4f2edb5ade360a193fcd08e",
+    "url": "/myStock/static/media/primeicons.c08d00ca.eot"
+  },
+  {
+    "revision": "c0929a1e055f2ceb5c889501c24a24e1",
+    "url": "/myStock/static/media/primeicons.c0929a1e.woff2"
+  },
+  {
+    "revision": "062070972fbad0f4fbb3505e6d013a39",
+    "url": "/myStock/static/media/primeicons.06207097.woff"
+  },
+  {
+    "revision": "acb19296cb26b15dcf42e13c603217f2",
+    "url": "/myStock/static/media/primeicons.acb19296.ttf"
+  },
+  {
+    "revision": "1d854e3ab2c37173804a0d1d1af229ee",
+    "url": "/myStock/static/media/primeicons.1d854e3a.svg"
+  },
+  {
+    "revision": "d26a61f5962c40392d82df5eea263851",
+    "url": "/myStock/index.html"
+  }
+];
